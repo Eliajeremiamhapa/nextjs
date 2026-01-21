49b118184ffc9a872665ac2528348f567a8129a5
Eliajeremiamhapa <eliamhapa34@gmail.com>
2026-01-21 10:41:36 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  reactStrictMode: true,
+
+  // 👇 FORCE WEBPACK (disable Turbopack)
+  turbopack: {},
+});
